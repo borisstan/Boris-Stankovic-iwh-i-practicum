6b7a0ddd32008ec88f75265243f568a0aab56c61
Boris Stankovic <stankovic.boris@outlook.com>
2024-04-25 14:22:17 +0200

    Saving new coffee entity, redirect to home page

--- index.js
+++ index.js
@@ -39,7 +39,31 @@ app.get("/update-cobj", async (req,res) => {
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
 // * Code for Route 3 goes here
+app.post("/update-cobj", async (req,res) =>{
+    const update = {
+        properties:{
+            "name":req.body.coffeeName,
+            "producer":req.body.producer,
+            "strength":req.body.strength,
+            "flavor":req.body.flavor
+        }
+    }
+    
+    const coffees =
+    "https://api.hubspot.com/crm/v3/objects/2-127789929"; 
 
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        "Content-Type": "application/json",
+      };
+
+    try {
+        await axios.post(coffees, update, {headers});
+        res.redirect("/");
+    } catch (error) {
+        console.error(error);
+    }
+});
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
 
