c13d1150efee744fbcd8f87e2c5baaa0be449cf6
Boris Stankovic <stankovic.boris@outlook.com>
2024-04-25 13:11:27 +0200

    API Call for fetching coffee objects without properties

--- index.js
+++ index.js
@@ -9,10 +9,25 @@ app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
 const PRIVATE_APP_ACCESS = process.env.APP_ACCESS;
-
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
+app.get('/', async (req,res)=>{
+    const coffees = 'https://api.hubspot.com/crm/v3/objects/2-127789929/';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    }
+
+    try {
+        const response = await axios.get(coffees, {headers});
+        const data = response.data.results;
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+    }
+});
+
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
